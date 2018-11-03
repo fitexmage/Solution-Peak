c9969aec04c49e82c183789c9fb87f627cab1f92
Shengguang Bai <fitexmage@126.com>
2018-11-03 01:56:51 -0400

    Add readme

--- Interview/20181103/Main.java
+++ Interview/20181103/Main.java
@@ -3,17 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package test;
 
 import java.util.Scanner;
-
-import java.util.*;
 
 /**
  *
  * @author fitexmage
  */
-public class Test {
+public class Main {
 
     /**
      * @param args the command line arguments
