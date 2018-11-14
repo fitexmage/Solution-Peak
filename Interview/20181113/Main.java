9c996df23d32b8253883c0dfae057d2c5a981880
Shengguang Bai <fitexmage@126.com>
2018-11-14 00:30:53 -0500

    Modify solution

--- Interview/20181113/Main.java
+++ Interview/20181113/Main.java
@@ -5,6 +5,7 @@ import java.util.List;
 //输入一个列表的单词，及一个长字符串，判断字符串是否能由列表中的单词组成。
 //补充条件：一个单词可以被使用多次；每个单词不必都被用上；不允许以单词重叠的方式组成字符串。
 public class Main {
+
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<String>();
         list.add("I");
@@ -27,17 +28,12 @@ public class Main {
             return false;
         }
 
-        for (int i = 0; i < list.size(); i++) {
-            String s = list.get(i);
-            list.remove(s);
-            String[] word_list = string.split(s);
-            boolean check = true;
-            for (String word : word_list) {
-                check = check && split(list, word);
-            }
-            list.add(i, s);
-            if (check) {
-                return true;
+        for (String s: list) {
+            String[] word_list = string.split(s, 2);
+            if (word_list[0].equals("")) {
+                if (split(list, word_list[1])) {
+                    return true;
+                }
             }
         }
         return false;
